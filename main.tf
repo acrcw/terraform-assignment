6538d41671fba0501b301643d15cf0c20f8ee748
joban <joban9872@gmail.com>
2024-01-25 19:08:58 +0530

    done

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "local_file" "file2" {
   content = <<-EOT
     This file contains Info about all the users
     Content of user_object_list
-    ${join("\n", [for i, user in var.user_object_list: "\nID: ${random_id.user_id_generator[i].hex},\nName: ${user.name},\nLPA: ${user.CTC},\nAddress: ${user.address},\nPassword: ${random_password.user_password[i].result}  ,\nVerified: ${user.verified} ,\nManager: ${user.manager} ,\nDesignation: ${user.designation} \nSkills: ${join("\t, ", user.skills)}\nDetails:\n${join("\n", [for k, v in user.details : "  ${k}: ${v}"])}"])}
+    ${join("\n", [for i, user in var.user_object_list: "\nID: ${random_pet.CEQ_id_generator[i].id}_${random_id.user_id_generator[i].hex},\nName: ${user.name},\nLPA: ${user.CTC},\nAddress: ${user.address},\nPassword: ${random_password.user_password[i].result}  ,\nVerified: ${user.verified} ,\nManager: ${user.manager} ,\nDesignation: ${user.designation} \nSkills: ${join("\t, ", user.skills)}\nDetails:\n${join("\n", [for k, v in user.details : "  ${k}: ${v}"])}"])}
 
   EOT
 }
@@ -25,6 +25,17 @@ resource "local_file" "file3" {
 
 
 
+resource "random_pet" "nickname_generator" {
+  count=length(var.user_object_list)
+  length = 1
+  prefix = "CEQ"
+}
+resource "random_pet" "CEQ_id_generator" {
+  count=length(var.user_object_list)
+  length = 1
+  prefix = "CEQ"
+  separator = "_"
+}
 resource "random_id" "user_id_generator" {
   count       = length(var.user_object_list)
   byte_length = 8
@@ -34,17 +45,18 @@ locals {
   current_user_id       = random_id.user_id_generator[0].hex
   current_user_nickname = var.current_user
   current_user_password = random_password.user_password[0].result
-  formatted_user_details = [for user in var.user_object_list : <<-EOT
+  formatted_user_details = [for i,user in var.user_object_list : <<-EOT
     User Details for ${user.name}:
     Name: ${user.name}
     CTC: ${user.CTC}
+    NickName: ${random_pet.nickname_generator[i].id}
     Address: ${user.address}
     Password: ${user.password}
     Verified: ${user.verified}
     Designation: ${user.designation}
     Skills: ${join(", ", user.skills)}
     Details:
-      ${join("\n", [for k, v in user.details : "${k}: ${v}"])}
+      ${join("\n", [for k, v in user.details : "${k}: ${v}\n"])}
   EOT
   ]
 }
